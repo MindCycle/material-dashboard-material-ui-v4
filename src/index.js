5c5531cba1811a36707c0ff3d674bee060490df8
Ishaan Pratap <ishaan@zezamii.com>
2022-11-15 11:30:03 +1000

    Updated all to match style and remove upgrade and rtl routes

--- src/index.js
+++ src/index.js
@@ -15,23 +15,23 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 // core components
-import Admin from "layouts/Admin.js";
-import RTL from "layouts/RTL.js";
+import Admin from 'layouts/Admin.js';
+import RTL from 'layouts/RTL.js';
 
-import "assets/css/material-dashboard-react.css?v=1.10.0";
+import 'assets/css/material-dashboard-react.css?v=1.10.0';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<Switch>
+			<Route path='/admin' component={Admin} />
+			<Route path='/rtl' component={RTL} />
+			<Redirect from='/' to='/admin/dashboard' />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById('root')
 );
